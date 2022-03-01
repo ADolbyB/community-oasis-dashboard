b7367d50ca2d6d2ad08e4a47cd4ec94aed9229f5
Michael Mesquita <mannolaur@gmail.com>
2022-03-01 18:23:34 -0500

    Linting with eslint

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import logo from "/src/logo.svg";
 import "./App.css";
 
+/**
+ * Starts the web application
+ * @return {App} Web Application Mount Point
+ */
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Check me out <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
