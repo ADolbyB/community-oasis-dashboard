b4c63e6c224148714235c7e06ea9a3ef6faf5f16
Michael Mesquita <mannolaur@gmail.com>
2022-03-28 10:36:43 -0400

    Linted files inside Cloud Functions

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   env: {
     es6: true,
-    node: true
+    node: true,
   },
   extends: [
     "eslint:recommended",
@@ -16,15 +16,15 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: ["tsconfig.json", "tsconfig.dev.json"],
-    sourceType: "module"
+    sourceType: "module",
   },
   ignorePatterns: [
-    "/lib/**/*" // Ignore built files.
+    "/lib/**/*", // Ignore built files.
   ],
   plugins: ["@typescript-eslint", "import"],
   rules: {
-    quotes: ["error", "double"],
+    "quotes": ["error", "double"],
     "import/no-unresolved": 0,
     "no-console": 1,
-  }
+  },
 };
