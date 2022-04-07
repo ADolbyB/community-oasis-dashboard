55bcce17ef9b8373ca58f391879883041649d13a
Hakeem Johnson <hakeemjohnson@Hakeems-MacBook-Pro.local>
2022-04-06 22:35:16 -0400

    my account page

--- src/pages/MyAccount.tsx
+++ src/pages/MyAccount.tsx
@@ -1,13 +1,68 @@
 import React from "react";
+import  TextField  from '@mui/material/TextField';
+import AdapterDatefns from '@mui/lab/AdapterDatefns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DatePicker from '@mui/lab/DatePicker';
+
+
+
+
 
 /**
  * A web page view for account management
  * @returns Web Page View
  */
 export default function MyAccount() {
+
+  const [value, setValue] = React.useState<Date | null>(null);
+
+
   return (
+    <React.Fragment>
+
+
+    
     <div>
-      <p>Im in</p>
+      <h1 className="Accout-Header"> My Account</h1>
+
+      <p>Welcome, if you are a new Resident please Select a date below:</p>
+
+      <LocalizationProvider dateAdapter={AdapterDatefns}>
+
+      <DatePicker
+        label="Select Orientation date"
+        value={value}
+        onChange={(newValue) => {
+          setValue(newValue);
+        }}
+        renderInput={(params) => <TextField {...params} />}
+      />
+      </LocalizationProvider>
+
     </div>
+
+    <div >
+
+      <p>Upcoming Events:</p>
+  
+
+
+    </div>
+
+
+
+
+    <div>
+
+      <p>Bus and Carpool information:</p>
+  
+
+
+    </div>
+
+
+
+    </React.Fragment>
+
   );
 }
