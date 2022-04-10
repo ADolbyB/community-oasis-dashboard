b0033a1b95a6bd56ad39ab57f502f22780b55fe5
josh1205 <68247933+josh1205@users.noreply.github.com>
2022-04-10 03:41:13 -0400

    Added user ability to add transactions to database (#36)

--- src/pages/Payment.tsx
+++ src/pages/Payment.tsx
@@ -1,6 +1,19 @@
 // React
 import React, {useState} from "react";
 
+// Auth
+import {useUserAuth} from "../contexts/UserAuthContext";
+
+// Firestore
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  serverTimestamp,
+  doc,
+} from "firebase/firestore";
+import app from "../firebase";
+
 // Layout
 import MainLayout from "../layouts/MainLayout";
 
@@ -15,6 +28,9 @@ import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 
+// Init firestore
+const db = getFirestore(app);
+
 const useStyles = makeStyles({
   content: {
     marginTop: 30,
@@ -28,6 +44,17 @@ const useStyles = makeStyles({
     marginTop: 40,
   },
 });
+
+const paymentTypes = [
+  {
+    value: "0",
+    label: "Initial Deposit",
+  },
+  {
+    value: "1",
+    label: "Quarterly Fee",
+  },
+];
 
 const cards = [
   {
@@ -51,7 +78,26 @@ const cards = [
 export default function Payment() {
   const classes = useStyles();
   const [card, setCard] = useState("");
+  const [paymentType, setPaymentType] = useState("");
+  const [date, setDate] = useState("");
+  const [amount, setAmount] = useState("");
+  const [note, setNote] = useState("");
+  const [cardNumber, setCardNumber] = useState("");
+  const [code, setCode] = useState("");
+  const {user} = useUserAuth();
 
+  const transactionData = {
+    date: serverTimestamp(),
+    payment: amount,
+    transaction_type: parseInt(paymentType),
+    user: doc(db, "users", user.uid),
+  };
+
+  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const transactionRef = collection(db, "transactions",);
+    await addDoc(transactionRef, transactionData);
+  };
   return (
     <MainLayout>
       <Header title="Make a Payment" />
@@ -59,9 +105,21 @@ export default function Payment() {
         <Box className={classes.content}>
           <Box className={classes.input}>
             <SelectInput
+              items={paymentTypes}
+              value={paymentType}
+              header="Type of payment*"
+              required={true}
+              onChange={
+                (event: React.ChangeEvent<HTMLInputElement>) =>
+                  setPaymentType(event.target.value)
+              }
+            />
+          </Box>
+          <Box className={classes.input}>
+            <SelectInput
               items={cards}
               value={card}
-              header="Type of payment*"
+              header="Card Type*"
               required={true}
               onChange={
                 (event: React.ChangeEvent<HTMLInputElement>) =>
@@ -71,34 +129,57 @@ export default function Payment() {
           </Box>
           <Box className={classes.input}>
             <TextInput
-              header="Payment Amount*"
-              placeholder="Amount"
+              header="Card number*"
+              placeholder="4444 4444 4444 4444"
               required={true}
-              onChange=""
+              onChange={
+                (event: React.ChangeEvent<HTMLInputElement>) =>
+                  setDate(event.target.value)
+              }
             />
           </Box>
           <Box className={classes.input}>
             <TextInput
-              header="Experation date*"
+              header="Expiration date*"
               placeholder="MM/YY"
               required={true}
-              onChange=""
+              onChange={
+                (event: React.ChangeEvent<HTMLInputElement>) =>
+                  setDate(event.target.value)
+              }
             />
           </Box>
           <Box className={classes.input}>
             <TextInput
               header="CVC*"
-              placeholder="CVC"
+              placeholder="000"
               required={true}
-              onChange=""
+              onChange={
+                (event: React.ChangeEvent<HTMLInputElement>) =>
+                  setCode(event.target.value)
+              }
             />
           </Box>
           <Box className={classes.input}>
             <TextInput
-              placeholder="Note/Memo"
+              header="Payment Amount*"
+              placeholder="$1500"
+              required={true}
+              onChange={
+                (event: React.ChangeEvent<HTMLInputElement>) =>
+                  setAmount(event.target.value)
+              }
+            />
+          </Box>
+          <Box className={classes.input}>
+            <TextInput
               header="Note/Memo"
+              placeholder="For future me"
               required={false}
-              onChange=""
+              onChange={
+                (event: React.ChangeEvent<HTMLInputElement>) =>
+                  setNote(event.target.value)
+              }
             />
           </Box>
           <Box className={classes.submit}>
@@ -106,7 +187,9 @@ export default function Payment() {
               <Button
                 variant="contained"
                 color="primary"
-                type="submit">
+                type="submit"
+                onClick={handleSubmit}
+              >
                 Submit
               </Button>
             </Grid>
