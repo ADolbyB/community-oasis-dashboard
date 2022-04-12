25b5ee099b4915d5115aa4d19130b13b3b9cbed1
Michael Mesquita <mannolaur@gmail.com>
2022-04-11 21:39:53 -0400

    Refactored function to break apart and commit to async/await method instead of aa/.then() mix, changed to sending back status codes.

--- functions/src/notifications.ts
+++ functions/src/notifications.ts
@@ -2,70 +2,66 @@ import * as functions from "firebase-functions";
 import { db } from "./admin";
 
 /**
- * Computes the set difference between two sets and returns the difference.
- * @param a  An set of strings
- * @param b A proper subset of a
- * @returns The difference between sets a and b.
+ * Get uid of users who haven't submitted a payment this quarter
+ * @returns Uid of users who haven't submitted a maintenance payment
  */
-function setDifference(a: Array<string>, b: Array<string>): Array<string> {
-  return a.filter((item) => !b.includes(item));
+async function getUnpaidUsers(): Promise<Array<string>> {
+  functions.logger.log("getUnpaidUsers() was called");
+
+  // Get which fiscal quarter we're currently in
+  const today = new Date();
+  const quarter: number = Math.floor((today.getMonth() + 3) / 3);
+
+  // Get start and end date of quarter
+  const startDate = new Date(today.valueOf());
+  startDate.setMonth((quarter - 1) * 3);
+  startDate.setDate(1);
+  startDate.setHours(0);
+  startDate.setMinutes(0);
+  startDate.setSeconds(0);
+
+  const endDate = new Date(startDate.valueOf());
+  endDate.setMonth(startDate.getMonth() + 3);
+
+  // Get all users who made qualifying transactions in this quarter
+  const paidUsers: Array<string> = [];
+
+  let query = db.collection("transactions").where("date", ">=", startDate);
+  query = query.where("date", "<", endDate);
+  query = query.where("transaction_type", "==", 1);
+
+  const querySnapshot = await query.get();
+  querySnapshot.forEach((doc) => {
+    const user = doc.get("user").id;
+    paidUsers.push(user);
+  });
+
+  // Get users from database who haven't made a transaction this quarter.
+  const unpaidUsers: Array<string> = [];
+  const usersCol = db.collection("users");
+  const updQuery = usersCol.where("__name__", "not-in", paidUsers);
+
+  const updQuerySnapshot = await updQuery.get();
+  updQuerySnapshot.forEach((doc) => {
+    const user = doc.id;
+    unpaidUsers.push(user);
+  });
+
+  functions.logger.log(`Unpaid users found: ${unpaidUsers}`);
+
+  return unpaidUsers;
 }
 
 export const sendQuarterlyFunction = functions.https.onRequest(
     async (req, res) => {
-      functions.logger.log("sendQuarterly Function has been activated.");
+      functions.logger.log("sendQuarterlyFunction() has been called.");
 
-      // Get which fiscal quarter we're currently in
-      const today = new Date();
-      const quarter: number = Math.floor((today.getMonth() + 3) / 3);
-
-      // Get start and end date of quarter
-      const startDate = new Date(today.valueOf());
-      startDate.setMonth((quarter - 1) * 3);
-      startDate.setDate(1);
-      startDate.setHours(0);
-      startDate.setMinutes(0);
-      startDate.setSeconds(0);
-
-      const endDate = new Date(startDate.valueOf());
-      endDate.setMonth(startDate.getMonth() + 3);
-
-      // Get all users who made qualifying transactions in this quarter
-      const paidUsers: Array<string> = [];
-
-      let query = db.collection("transactions").where("date", ">", startDate);
-      query = query.where("date", "<", endDate);
-      query = query.where("transaction_type", "==", 1);
-
-      query.get().then((querySnapshot) => {
-        querySnapshot.forEach((documentSnapshot) => {
-          const user = documentSnapshot.data()["user"].id;
-          functions.logger.log(`Found transaction for ${user}`);
-          paidUsers.push(user);
-        });
-      });
-
-      functions.logger.log(paidUsers);
-      functions.logger.log("THis works well");
-
-      // Get all users from the database
-      const allUsers: Array<string> = [];
-
-      db.collection("users").get().then((querySnapshot) => {
-        querySnapshot.forEach((documentSnapshot) => {
-          const user = documentSnapshot.id;
-          allUsers.push(user);
-        });
-      });
-
-      // functions.logger.log(paidUsers);
-      // functions.logger.log(allUsers);
-      // functions.logger.log(unpaidUsers);
-
-      // Get unpaid users using set difference
-      const unpaidUsers = setDifference(allUsers, paidUsers);
-
-
-      res.send("These users have not paid their maintenance fees yet:\n" +
-          [ ...unpaidUsers ]);
+      try {
+        const unpaidUsers = await getUnpaidUsers();
+        functions.logger.log(`Sending payment notification to: ${unpaidUsers}`);
+        res.sendStatus(200);
+      } catch (error) {
+        functions.logger.error("Unknown Error Occurred:" + error);
+        res.sendStatus(500);
+      }
     });
